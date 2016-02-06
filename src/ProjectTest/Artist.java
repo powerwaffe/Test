7cdb13b7ad25de179eabdee1ec588b1f89bacad7
Sean <powerwaffe@gmail.com>
2016-02-06 01:17:08 -0500

    Sean's Programs 2/1/2016

--- src/ProjectTest/Artist.java
+++ src/ProjectTest/Artist.java
@@ -10,10 +10,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 
-/**
- * Created by Sean on 2/1/2016.
- */
-
 public class Artist {
     private String artistName;
     private int artistID;
@@ -47,117 +43,95 @@ public class Artist {
      * MODIFY TO DETECT INCORRECT ID
      */
     public static void main(String[] args) {
-        //Array list to be loaded
-        //ArrayList artistArrayList = new ArrayList();
-        ArrayList<Object> tokens = new ArrayList<>();
+        ArrayList<String> tokens = new ArrayList<>();
         ArrayList<String> artistArrName = new ArrayList<>();
-        //String[] myArr = new String[120];
         int counter = 0; //counter for lines
+        String[] arrayTry = new String[116];
 
-        /**maybe create dummy array list with large size ex. arr[300]*/
-
-        try {
+        try
+        {
             Scanner input = new Scanner(new File("p1artists.txt"));
             File myFile = new File("p1artists_out1.txt");
             PrintWriter output = new PrintWriter(myFile);
-
-            while (input.hasNext()) {
-                // artistArrList.add(art.next()); //load p1artists.txt into artistArrList
+            int ccc=0;
+            while (input.hasNext())
+            {
                 counter++;
-                //nameArray.add(art.nextLine());
-                try {
+                try
+                {
                     int artistID = input.nextInt();
                     String artistName = input.next();
+                    arrayTry[ccc] = artistName;
                     output.println(artistID + " " + artistName);
                     System.out.println(artistID + " " + artistName);
-                } catch (InputMismatchException e) {
+                    ccc++;
+                }
+                catch (InputMismatchException e)
+                {
                     input.next();
-                   // output.close(); //close PrintWriter
                 }
             }
             output.close(); //close PrintWriter
-            input.close();
-        } catch (FileNotFoundException e) {
+            input.close(); //close Scanner
+        } //end try
+        catch (FileNotFoundException e)
+        {
             System.out.println("No such file was found");
-        }
+        } //end catch
 
-
-        try {
+        try
+        {
             Scanner input = new Scanner(new File("p1artists.txt"));
-            File myFile = new File("p1arts_out.txt");
-            PrintWriter output = new PrintWriter(myFile);
 
             while (input.hasNext())
             {
-                tokens.add(input.nextLine());
+                tokens.add(input.next());
             }
-        } catch (FileNotFoundException e) {
+        } //end try
+        catch (FileNotFoundException e)
+        {
             e.printStackTrace();
-        }
+        } //end catch
 
         /**Write all to new file*/
-        try {
+        int loop = 1;
+        int artCounter = 0;
+        try
+        {
             Scanner input = new Scanner(new File("p1arts.txt"));
             File myFile = new File("p1arts_out.txt");
             PrintWriter output = new PrintWriter(myFile);
-
             while (input.hasNext()) {
                 try {
-                    String artwork = input.next();
+                    int art = input.nextInt();
+                    String names = input.nextLine();
+                    output.println(art + names + "\t" + arrayTry[loop-1] + "\n");
+                    loop++;
+                    artCounter++;
 
-                    output.println(artwork);
-                    for (int i =0; i < tokens.size(); i++)
-                        output.println(tokens.get(i));
                 } catch (InputMismatchException e) {
                     input.next();
-                    // output.close(); //close PrintWriter
                 }
             }
             output.close(); //close PrintWriter
             input.close();
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e)
+        {
             System.out.println("No such file was found");
         }
 
-        //e.printStackTrace();
-        // Artist[] items = {new Artist(1, "Acconci"),
-        //   new Artist(2, "Ames"),
-        // new Artist(3, "Aserty"),
-        //  new Artist(4, "Baron"),
-        //   new Artist(5, "Battenberg")};
-        //for (int i = 0; i < items.length; i++)
-        //System.out.println(items[i]);
-        //  for (int i=0; i < artistArrList.size(); i++)
-        // {
-        // System.out.println(artistArrList);
-
         /**Array tester*/
-        //for (int i = 0; i < artistArrayList.size(); i++)
-        //   System.out.print(artistArrayList.get(i).toString() + "\n");
         int newCounter = counter-1;
         System.out.println("\nAmount of artists " + newCounter);
-        //String[] tokenArray = tokens.toArray(new String[0]);
+        System.out.println("Total amount of artwork " + artCounter);
 
         /**Array tester*/
-        System.out.println("Array contents:\n");
-        for (int i = 0; i< tokens.size(); i++)
-        System.out.println(tokens.get(i));
-          //  System.out.println(tokenArray[i]);
-        //System.out.println(artistArray.length);
-        /**Print to file*/
-       // try {
-           // Scanner input = new Scanner(new File("p1artists.txt"));
-          //  while (input.hasNext())
-        //    {
-           //    artistArray[counter-1]= input.nextLine();
-         //   }
+        //System.out.println("Array contents:\n");
+        for (int i = 0; i< arrayTry.length; i++)
+            System.out.println(arrayTry[i]);
 
-      //  } catch (FileNotFoundException e) {
-        //    e.printStackTrace();
-      //  }
-      //  System.out.println("My array!");
-        //for (int i=0;i<artistArray.length;i++)
-           // System.out.println(artistArray[i]);
+        System.out.println(arrayTry.length);
     }
 }
 
